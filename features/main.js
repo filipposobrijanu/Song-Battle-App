e13a5621ce7aeb91308dabc447daa4e445e22458
Fillipos Obrijanu <150919392+filipposobrijanu@users.noreply.github.com>
2025-06-24 13:20:44 +0300

    Add files via upload

---
+++ features/main.js
@@ -0,0 +1,114 @@
+//var song = ["https://open.spotify.com/embed/track/631jkOX5M1tanSVc48QsJT", "https://open.spotify.com/embed/track/1ZJHpXtC2gaNtaDx1II9Ny", "https://open.spotify.com/embed/track/6PonePtFLgFR45B7MgxpX7", "https://open.spotify.com/embed/track/4CwQmratLbi7lB1rwtWLGm"];
+
+var song = ["https://open.spotify.com/embed/track/631jkOX5M1tanSVc48QsJT", "https://open.spotify.com/embed/track/1ZJHpXtC2gaNtaDx1II9Ny", "https://open.spotify.com/embed/track/6PonePtFLgFR45B7MgxpX7", "https://open.spotify.com/embed/track/4CwQmratLbi7lB1rwtWLGm", "https://open.spotify.com/embed/track/4p9hsvM4y5pQKsttQSX9qb", "https://open.spotify.com/embed/track/6sh306dkqv89n2r1OsnCc9", "https://open.spotify.com/embed/track/6uFG0BRd3BaonWjEGuHYAh", "https://open.spotify.com/embed/track/40Z5DtW90lXSR0R23EQLla", "https://open.spotify.com/embed/track/51xpNYObJdiEnbvpjoAE8E", "https://open.spotify.com/embed/track/5w3ghjBUzRCFJHAAqOAkb1", "https://open.spotify.com/embed/track/1xilbbJ1WSlivvtkfYwhSQ", "https://open.spotify.com/embed/track/5i5vQLXtbLLuQWGks9RHyz", "https://open.spotify.com/embed/track/03KgYnNIp4EOgPT2hS93QF", "https://open.spotify.com/embed/track/3yWIyyt0ETjTeEkumKpHX8", "https://open.spotify.com/embed/track/69fBXqAmhOfp7NCSwEsd1h", "https://open.spotify.com/embed/track/6CtOglDAA4HB0nkZmkdGJe", "https://open.spotify.com/embed/track/4xXTPNQ2BjbVzWsbLGiMn3", "https://open.spotify.com/embed/track/6lqRjNaL63nmwHbQcekxLh", "https://open.spotify.com/embed/track/76fNtlXyyeT3rxP5VFFocj", "https://open.spotify.com/embed/track/7qPiAhk71D2RqLRtnjDL76", "https://open.spotify.com/embed/track/2fpTRcMLt4aKlqULOwuXUP", "https://open.spotify.com/embed/track/0r9ZhD7QEbIAcvfhDkIsdV", "https://open.spotify.com/embed/track/3Zm7eNNzIEXzlMBeh0uwWj", "https://open.spotify.com/embed/track/00zk0uua6s2ifh0Nc3ppfW", "https://open.spotify.com/embed/track/1HXajOe3oHqAh10XFA2R9M", "https://open.spotify.com/embed/track/4j3sLMVOMeJKnW7dhSBbJZ", "https://open.spotify.com/embed/track/0XXKBSqgvxNmj4315mJirM", "https://open.spotify.com/embed/track/7HNNxPQDzRWy1gAFV8AyDW", "https://open.spotify.com/embed/track/38oiUIjXdawwEa4Ty63Mdm", "https://open.spotify.com/embed/track/6yTy7zuNnUglg5EIlD5vX4", "https://open.spotify.com/embed/track/7sjhOYcPgeSv3oj3IkFTeK", "https://open.spotify.com/embed/track/4wTXKpGKv73CBKcip15S00", "https://open.spotify.com/embed/track/3HqGDrSOZ6LbBfX5EsiaAj", "https://open.spotify.com/embed/track/4Q05V5rx4Uo2298kyAXvqM", "https://open.spotify.com/embed/track/1cByP16xx3Bikp74r1TW5f", "https://open.spotify.com/embed/track/2VT3E35HDDCvvxcLGQpVWS", "https://open.spotify.com/embed/track/62A4XU17hmUbaHu4s2gIqq", "https://open.spotify.com/embed/track/0XgQbY9WvAbIQRfVWzRPBK", "https://open.spotify.com/embed/track/1a019wP7IdYLexwbmfZPm3", "https://open.spotify.com/embed/track/78MPwJNNwlQMbKLbF1kxKN", "https://open.spotify.com/embed/track/6IyoLWzljeR3ldQo4KWHT6", "https://open.spotify.com/embed/track/6kkGAeGS3oohGek9gje6KJ", "https://open.spotify.com/embed/track/288YEUQfI3Hwrx8z54lHkp", "https://open.spotify.com/embed/track/1HqKqHh5iKQzz9LhQDlbgm", "https://open.spotify.com/embed/track/6vddKOUj2Fx4LEAYxmsT8p", "https://open.spotify.com/embed/track/0oO1AbL92F6329z13C2qQH", "https://open.spotify.com/embed/track/4XKrI8AA0FOiwMMZ4bjX4s", "https://open.spotify.com/embed/track/5aO9gzffBoElavmuQKDxgB", "https://open.spotify.com/embed/track/64DMD3OKUognShFuYJ5xMY", "https://open.spotify.com/embed/track/1DKtg0yijcSoUQTxkxixyi", "https://open.spotify.com/embed/track/3wn8nOygkHLUQ9dlXM1rKW", "https://open.spotify.com/embed/track/6OSjW5fVkuBeBdJ7mAzDyE", "https://open.spotify.com/embed/track/6CZCmIHLKm9lK66j5xDt0X", "https://open.spotify.com/embed/track/4J7IUiMAbCEBN15c2ekiDh", "https://open.spotify.com/embed/track/3NRpNPJmhZF6VbSr769oNn", "https://open.spotify.com/embed/track/0Pq8CSHDXInMK8atOkWSAv", "https://open.spotify.com/embed/track/1cfhlm0L8NXdQW6h8iMjxN", "https://open.spotify.com/embed/track/37nYSBS0MfRCm913QwCCYo", "https://open.spotify.com/embed/track/70B3eWw0ivRKxEvbbHQcjs", "https://open.spotify.com/embed/track/0upTWhU4O3eBKECSdnF8ZG", "https://open.spotify.com/embed/track/6SKA4zY0zzYFdM74F2mPq1", "https://open.spotify.com/embed/track/6HgnSE2dp85gUO3KVgJL3b", "https://open.spotify.com/embed/track/0MxEnGqgMlVZn0lD1k61kR", "https://open.spotify.com/embed/track/7g6Br6y9YuOAofhGhWUuzc", "https://open.spotify.com/embed/track/7m4fQgZSrRg7lskCXWxbro", "https://open.spotify.com/embed/track/6UWngNZoZIEOzJs2v6ocnV", "https://open.spotify.com/embed/track/2aJs9c6FxOJh3ozQRdafcM", "https://open.spotify.com/embed/track/5H0FL4BOhNDQNCINAL3TO7", "https://open.spotify.com/embed/track/0SHO7NVnp3U5NFc9YWkPCR", "https://open.spotify.com/embed/track/1dyOKsLb0YcEzJcOxVJLpD", "https://open.spotify.com/embed/track/7EC3AUEj6jYFST3tyLZ4ly", "https://open.spotify.com/embed/track/6GukTiiz6sGrXEHPEqsD5Y", "https://open.spotify.com/embed/track/1zX4htZC4Xj60xoCM9RlX0", "https://open.spotify.com/embed/track/4QDkb8jDDi2M9MWyQbF6UL", "https://open.spotify.com/embed/track/0A7lEsDtaJ6jw5r2lGaIhI", "https://open.spotify.com/embed/track/6Qvasm9orB6bo3YZ1orw4S", "https://open.spotify.com/embed/track/5RZAzIS7ZM8vU2jcEGYJl6", "https://open.spotify.com/embed/track/6Rxs3rW1SNNBZRrRckwU81", "https://open.spotify.com/embed/track/4e9j7j0wKtPI7rc00wLH5b", "https://open.spotify.com/embed/track/4Ub9rNZn644QkrTdML26by", "https://open.spotify.com/embed/track/3sDhyDKFQ3X8FQSm2zy5Jo", "https://open.spotify.com/embed/track/5G9CRx8UPvVfZe1L8gXKT0", "https://open.spotify.com/embed/track/1aztkqMhtL4XSuGASmEeCo", "https://open.spotify.com/embed/track/6uk3Owf0rYkrdhv2eyXaw3", "https://open.spotify.com/embed/track/1XGJkd545elGBmcNTHMjOD", "https://open.spotify.com/embed/track/4mqsffIsjada5FIbI6cuNJ", "https://open.spotify.com/embed/track/2FRKyoQdvr86WJiMZxX2J7", "https://open.spotify.com/embed/track/0hMoGEOU0ogjOAJCzMAczN", "https://open.spotify.com/embed/track/6vmRtsvnmfABDSE7eqeNgW", "https://open.spotify.com/embed/track/5dA0iENzuQGYKZpCKU4nnN", "https://open.spotify.com/embed/track/1Z1KwftC6aS04IkgcY96uT", "https://open.spotify.com/embed/track/4AGE3bSRCwmTI5Jf8Wse3C", "https://open.spotify.com/embed/track/0rS311gRYwbJGbuwnousMD", "https://open.spotify.com/embed/track/2hzr5wWHJySmcPpGfF2mu1", "https://open.spotify.com/embed/track/3Mq0oy9rLoyu6OEN10nbBt", "https://open.spotify.com/embed/track/5swUIbecpzLt2yR8gTsx0b", "https://open.spotify.com/embed/track/7LHuQelec2bU928dviAfrA", "https://open.spotify.com/embed/track/4cmdJvxboUzxnlytGZTRXm", "https://open.spotify.com/embed/track/51RIAHpLbfGTr1PKE8z2O1", "https://open.spotify.com/embed/track/09gdNGvF4fXD2EyrIOayhw", "https://open.spotify.com/embed/track/4HWkMODEyeHO67sTkOgiB9", "https://open.spotify.com/embed/track/0rHe0Knbdx8bWEzQfUPOt1", "https://open.spotify.com/embed/track/0Y0OWxuw6IqvgXcm6HPaaz", "https://open.spotify.com/embed/track/0UbkjUkWEDTr0LkEwgLULe", "https://open.spotify.com/embed/track/0hAsJPztpM9wWNB17EneiJ", "https://open.spotify.com/embed/track/50JUGk1EFKIRLUt29qT1vF", "https://open.spotify.com/embed/track/7DnJSQuXftQga8DmzEouT2", "https://open.spotify.com/embed/track/4lOCoWyyqio7NfoJEV6d47", "https://open.spotify.com/embed/track/5MVSKeVoFWD5jxreGQLVkq", "https://open.spotify.com/embed/track/2IzGoP9Wz482rU6pW9YGmG", "https://open.spotify.com/embed/track/5PyjS9OsRJR0vEbFd7I6HA", "https://open.spotify.com/embed/track/7uhS6ogyKjkbxFcpvwImKm", "https://open.spotify.com/embed/track/6xdQYIkKZF8im3Npk9uxlk", "https://open.spotify.com/embed/track/1sLvhaIEDe79vHcSmbQwgm", "https://open.spotify.com/embed/track/4eAv0EtLgzReVecWhGcspv", "https://open.spotify.com/embed/track/6DLJNOM4ZDDtqeZVJjGpOn", "https://open.spotify.com/embed/track/2lJAPa5W0XvCLpVtGXl9jg", "https://open.spotify.com/embed/track/3AmxGBTGJofdN5cXn2WiTf", "https://open.spotify.com/embed/track/4zAyaaEMYSa218q4wpJIP9", "https://open.spotify.com/embed/track/1Ii5TnQwr5ViOyhK2J3sMv", "https://open.spotify.com/embed/track/6aFICfnNnEDfiEs4AqYdpC", "https://open.spotify.com/embed/track/6eZameJLXaey8Q6RUTjxh7", "https://open.spotify.com/embed/track/53I0NWZV139riivg59gQeO", "https://open.spotify.com/embed/track/2XdCMQNt0BTKfQEM6wODAG", "https://open.spotify.com/embed/track/510MJD1EXbMREQyrCKbUhr", "https://open.spotify.com/embed/track/36RhHU4GZBHudh9Yrv9Ovi", "https://open.spotify.com/embed/track/3jQ9t9u2GypQYTLLXngKXm", "https://open.spotify.com/embed/track/2Wi0iXageSUjh7SZXNRP1m"];
+
+var Wsongs = [];
+
+var temp = 1;
+
+var rounds = 1;
+var roundsend = 64;
+
+let randnumber = Math.floor(Math.random() * (song.length));
+
+var d45 = document.getElementById('d45');
+
+var rounds_p = document.getElementById("rounds");
+var d1 = document.getElementById('d1');
+d1.src = song[randnumber];
+song.splice(song.indexOf(d1.src),1);
+temp++;
+randnumber = Math.floor(Math.random() * (song.length));
+var d2 = document.getElementById('d2');
+d2.src = song[randnumber];
+song.splice(song.indexOf(d2.src),1);
+temp++;
+
+
+function Next(n){
+    
+    if(rounds==roundsend){
+
+        rounds=0;
+        roundsend = roundsend/2;
+        song = Wsongs;
+        Wsongs = [];
+        temp = 0;
+        if(roundsend==0.5){
+
+            EndGame();
+            roundsend = 0;
+
+        }
+        d45.src = song[0];
+        console.log(song.length);
+
+    }
+    $("#d1").fadeOut(500);
+    $("#d2").fadeOut(500);
+
+    if(n==1){
+
+        Wsongs.push(d1.src);
+
+    }
+    else{
+
+        Wsongs.push(d2.src);
+
+    }
+    rounds++;
+    $("#d1").fadeIn(300);
+    $("#d2").fadeIn(300);
+    setTimeout(function() {
+
+        randnumber = Math.floor(Math.random() * (song.length));
+        d1.src = song[randnumber];
+        song.splice(song.indexOf(d1.src),1);
+        temp++;
+        randnumber = Math.floor(Math.random() * (song.length));
+        d2.src = song[randnumber];
+        song.splice(song.indexOf(d2.src),1);
+        temp++;
+        
+    }, 400);
+
+
+    rounds_p.innerHTML = rounds + "/" + roundsend;
+}
+function StartGame(){
+    
+    $(".startingscreen").fadeOut(1000);
+    setInterval(function(){
+
+        $(".startingscreen").hide();
+        $(".hide").fadeIn(1000);
+
+    },1000)
+    setInterval(function(){
+
+        $(".hide").show(1000);
+
+    },1000)
+}
+function EndGame(){
+
+    
+    $(".hide").fadeOut(1000);
+    setInterval(function(){
+
+        $(".hide").hide();
+        $(".endscreen").fadeIn(1000);
+
+    },1000)
+    setInterval(function(){
+
+        $(".endscreen").show(1000);
+
+    },1000)
+
+}
+function Refresh(){
+
+    location.reload();
+}
